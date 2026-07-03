0493750a607b42310e592d1fbe33c2cb8c60b2af
manwarj <mohamedanouarj@gmail.com>
2026-07-03 11:36:17 +0200

    updated forgot-pwd email 2

--- config/forgot-pwd-email.js
+++ config/forgot-pwd-email.js
@@ -10,7 +10,7 @@ module.exports = async (userEmail, firstName, id, origin, resetToken) => {
       subject: "Restore your Password",
       html: `<p>Hello <strong>${firstName}</strong>,</p>
       <p>You requested to reset your password. Click the link below:</p>
-      <a href="${origin}/reset-pwd/${resetToken}" target="_blank" >Reset Password</a>
+      <a href="${origin}/reset-password/${resetToken}" target="_blank" >Reset Password</a>
       <p>This link expires in 15 minutes.</p>
       <p>If you did not request this, ignore this email.</p>
       `,
